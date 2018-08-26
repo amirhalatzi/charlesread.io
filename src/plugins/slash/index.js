d261915e945a6985a60290ed74e46769e15a7966
Charles Read <charles@charlesread.io>
2018-08-25 21:32:11 -0400

    debugging

--- src/plugins/slash/index.js
+++ src/plugins/slash/index.js
@@ -2,24 +2,25 @@
 
 const address = require('ip').address()
 const jwt = require('jsonwebtoken')
+const moment = require('moment-timezone')
 
 module.exports = function (fastify, opts, next) {
   fastify.get('/', (req, reply) => {
     const payload = {
       message: 'welcome to charlesread.io',
-      itis: new Date(),
+      itis: moment.tz(new Date(), 'America/New_York').format('LLLL z'),
       iam: address,
       repo: 'https://github.com/charlesread/charlesread.io',
-      image: 'https://hub.docker.com/r/charlesread/charlesread.io',
-      cookies: req.cookies
+      image: 'https://hub.docker.com/r/charlesread/charlesread.io'
     }
     if (req.cookies.token) {
       const tokenDecoded = jwt.decode(req.cookies.token, {complete: true})
+      payload.token = req.cookies.token
       payload.tokenDecoded = tokenDecoded
-      payload.jwtExpiration = new Date(tokenDecoded.payload.exp * 1000)
+      payload.jwtIssued = moment.tz(tokenDecoded.payload.iat * 1000, 'America/New_York').format('LLLL z')
+      payload.jwtExpiration = moment.tz(tokenDecoded.payload.exp * 1000, 'America/New_York').format('LLLL z')
     }
     if (req.credentials) {
-      payload.credentials = req.credentials
       payload.hey = req.credentials.sub
     } else {
       payload.login = 'https://www.charlesread.io/login'
