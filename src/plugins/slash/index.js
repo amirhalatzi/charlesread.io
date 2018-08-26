7dc5bbc35d071f7e7d80498c64fe8bcd61c9a5ea
Charles Read <charles@charlesread.io>
2018-08-25 20:41:46 -0400

    debugging

--- src/plugins/slash/index.js
+++ src/plugins/slash/index.js
@@ -13,6 +13,7 @@ module.exports = function (fastify, opts, next) {
       cookies: req.cookies
     }
     if (req.credentials) {
+      fastify.credentials.trace(req.credentials)
       payload.credentials = req.credentials
       payload.hey = req.credentials.sub
     } else {
