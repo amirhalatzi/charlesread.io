e98dda86740017a56f7761cdce455677ccd9e7f2
Charles Read <charles@charlesread.io>
2018-08-25 20:47:55 -0400

    debugging

--- src/plugins/slash/index.js
+++ src/plugins/slash/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const address = require('ip').address()
+const jwt = require('jsonwebtoken')
 
 module.exports = function (fastify, opts, next) {
   fastify.get('/', (req, reply) => {
@@ -11,6 +12,9 @@ module.exports = function (fastify, opts, next) {
       repo: 'https://github.com/charlesread/charlesread.io',
       image: 'https://hub.docker.com/r/charlesread/charlesread.io',
       cookies: req.cookies
+    }
+    if (req.cookies.token) {
+      payload.tokenDecoded = jwt.decode(req.cookies.token, {complete: true})
     }
     if (req.credentials) {
       fastify.credentials.trace(req.credentials)
