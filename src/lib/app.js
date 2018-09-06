6202ffbaad62b8c3e98d6882297d2157802bac18
Charles Read <charles@charlesread.io>
2018-09-05 20:37:02 -0400

    stuff

--- src/lib/app.js
+++ src/lib/app.js
@@ -6,15 +6,44 @@ const config = require('~/config.js')
 const log = require('~/lib/logger.js')()
 const fjwt = require('fastify-jwt-webapp')
 
-const dir = require('node-dir')
 const fastify = require('fastify')({
   logger: log,
   level: 'trace'
 })
 
+// tells marko to not output .js "temp" files
+require('marko/compiler').defaultOptions.writeToDisk = false
+const lasso = require('lasso')
+const dir = require('node-dir')
+
 const app = {}
 
 app.start = async function () {
+
+  // bundle up all CSS, LESS, and JS assets
+  lasso.configure({
+    outputDir: path.join(__dirname, '..', 'public', 'static'),
+    urlPrefix: '/public/static',
+    plugins: ['lasso-marko', 'lasso-less']
+  })
+  log.trace('finished lasso initialization')
+
+// register templating engine; marko
+  await fastify.register(require('point-of-view'), {
+    engine: {
+      marko: require('marko')
+    },
+    includeViewExtension: true
+  })
+  log.trace('finished marko initialization')
+
+// serve all the static files; all of /public
+  await fastify.register(require('fastify-static'), {
+    root: path.join(__dirname, '..', 'public'),
+    prefix: '/public/',
+  })
+  log.trace('finished static files initialization')
+
 
 // loop through all files in /plugins and fastify.register(require()) them
   const pluginPaths = await dir.promiseFiles(path.join(__dirname, '..', 'plugins'))
--- src/plugins/slash/index.js
+++ src/plugins/slash/index.js
@@ -6,23 +6,30 @@ const moment = require('moment-timezone')
 
 module.exports = function (fastify, opts, next) {
   fastify.get('/', (req, reply) => {
-    const payload = {
-      message: 'welcome to charlesread.io',
-      iam: address,
-      repo: 'https://github.com/charlesread/charlesread.io',
-      image: 'https://hub.docker.com/r/charlesread/charlesread.io'
-    }
+    const payload = {}
     if (req.credentials) {
       payload.hey = req.credentials.sub
-    } else {
+    }
+
+    payload.message = 'welcome to charlesread.io'
+
+    if (!req.credentials) {
       payload.login = 'https://www.charlesread.io/login'
     }
+
+    payload.ifYoureCurious = {
+      repo: 'https://github.com/charlesread/charlesread.io',
+      image: 'https://hub.docker.com/r/charlesread/charlesread.io',
+      containerAddress: address
+    }
+
     if (req.cookies.token) {
       const tokenDecoded = jwt.decode(req.cookies.token, {complete: true})
       payload.jwtIssued = moment.tz(tokenDecoded.payload.iat * 1000, 'America/New_York').format('LLLL z')
       payload.jwtExpiration = moment.tz(tokenDecoded.payload.exp * 1000, 'America/New_York').format('LLLL z')
     }
-    reply.send(payload)
+    reply.view('/pages/slash/index', {payload})
+    // reply.send(payload)
   })
   next()
 }
